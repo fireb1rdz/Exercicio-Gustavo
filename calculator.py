b2b42f5d299934197637946054ab8647ca83dc3c
Gustavo Paganelli <123592808+fireb1rdz@users.noreply.github.com>
2023-09-15 19:25:53 -0300

    Adding features subtração, divisão e multiplicação to develop branch (#4)
    * feature: adding adicao function
    
    * feature: adding subtração function
    
    * feature: adding adição function
    
    * feature: adding subtração function
    
    * feature: adding divisao function
    
    * feature: adding multiplicação function
    
    * Update calculator.py
    
    Co-authored-by: Jimmy Gouraud <jimmy.gouraud@hotmail.fr>
    
    ---------
    
    Co-authored-by: Jimmy Gouraud <jimmy.gouraud@hotmail.fr>

---
+++ calculator.py
@@ -0,0 +1,49 @@
+def adicao(n1: int, n2: int) -> int:
+    """Realiza a soma de dois números
+
+    Args:
+        n1: primeiro número.
+        n2: segundo número.
+
+    Returns:
+        A soma dos números.
+    """
+    return n1 + n2
+
+
+def subtracao(n1: int, n2:int) -> int:
+    """Realiza a subtração de dois números
+
+    Args:
+        n1: primeiro número.
+        n2: segundo número.
+
+    Returns:
+        A subtração dos números.
+    """
+    return n1 - n2
+
+def divisao(n1: float, n2: float) -> float:
+    """Realiza a divisão de dois números
+
+    Args:
+        n1: primeiro número.
+        n2: segundo número.
+
+    Returns:
+        O quociente dos números.
+    """
+    return n1 / n2
+
+
+def multiplicacao(n1: int, n2: int) -> int:
+    """Realiza a multiplicação de dois números
+
+        Args:
+            n1: primeiro número.
+            n2: segundo número.
+
+        Returns:
+            O produto dos números.
+        """
+    return n1 * n2
