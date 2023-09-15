a5311b5ff9cfecfadbc4983d51bf7c4c63c112b3
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-09-15 18:35:48 -0300

    feature: adding subtração function

--- calculator.py
+++ calculator.py
@@ -9,3 +9,16 @@ def adicao(n1: int, n2: int) -> int:
         A soma dos números.
     """
     return n1 + n2
+
+
+def subtracao(n1: int, n2:int) -> int:
+    """Realiza a subtração de dois números
+
+    Args:
+        n1: primeiro número.
+        n2: segundo número.
+
+    Returns:
+        A subtração dos números.
+    """
+    return n1 - n2
