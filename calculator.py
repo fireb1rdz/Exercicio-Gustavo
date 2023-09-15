cd115618bde8d4f2d7268f70c59d96a279443326
Gustavo N. Paganelli <gustavonelsonpaganelli@gmail.com>
2023-09-14 22:36:29 -0300

    feature: adding adicao function

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def adicao(n1: int, n2: int) -> int:
+    """Realiza a soma de dois números
+
+    Args:
+        n1: primeiro número.
+        n2: segundo número.
+
+    Returns:
+        A soma dos números.
+    """
+    return n1 + n2
